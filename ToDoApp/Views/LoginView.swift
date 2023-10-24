22a2f133157aa4b35b337a1d60aa8a08ba206cfc
AleksandraRusak <rusakaleksandra94@gmail.com>
2023-10-25 00:15:11 +0200

    Done LoginView design, added images

--- ToDoApp/Views/LoginView.swift
+++ ToDoApp/Views/LoginView.swift
@@ -6,3 +6,76 @@
 //
 
 import SwiftUI
+
+struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        VStack{
+            // Header
+            ZStack {
+                Image("login")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    Text("To Do List")
+                        .font(.system(size: 50))
+                        .foregroundColor(Color.white)
+                        .bold()
+                    
+                    Text("Get things done")
+                        .font(.system(size: 30))
+                        .foregroundColor(Color.white)
+                    
+                }
+                .padding(.top, -140)
+            }
+            .frame(width: UIScreen.main.bounds.width, height: 300)
+            
+            // Form
+            Form {
+                TextField("Email Address", text: $email)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.top, 5)
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.top, 5)
+                
+                Button {
+                    // Log in
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(.purple)
+                        
+                        Text("Log in")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                }
+            }
+            
+            // Create Account
+            VStack {
+                Text("New around here?")
+                Button("Create An Account") {
+                    // Show registration
+                }
+            }
+            .padding(.bottom, 30)
+            
+            Spacer()
+        }
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
+
