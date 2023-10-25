6aa5b263ad45074f378a1a06b8570195f0d842e0
AleksandraRusak <rusakaleksandra94@gmail.com>
2023-10-25 14:09:28 +0200

    done RegisterView design, added GeometryReader

--- ToDoApp/Views/LoginView.swift
+++ ToDoApp/Views/LoginView.swift
@@ -8,67 +8,67 @@
 import SwiftUI
 
 struct LoginView: View {
-    
     @State var email = ""
     @State var password = ""
-    
+
     var body: some View {
-        VStack{
-            // Header
-            ZStack {
-                Image("login")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .edgesIgnoringSafeArea(.all)
-                
-                VStack {
-                    Text("To Do List")
-                        .font(.system(size: 50))
-                        .foregroundColor(Color.white)
-                        .bold()
-                    
-                    Text("Get things done")
-                        .font(.system(size: 30))
-                        .foregroundColor(Color.white)
-                    
-                }
-                .padding(.top, -140)
-            }
-            .frame(width: UIScreen.main.bounds.width, height: 300)
-            
-            // Form
-            Form {
-                TextField("Email Address", text: $email)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.top, 5)
-                SecureField("Password", text: $password)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding(.top, 5)
-                
-                Button {
-                    // Log in
-                } label: {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 10)
-                            .foregroundColor(.purple)
-                        
-                        Text("Log in")
-                            .foregroundColor(.white)
+        GeometryReader { geometry in // Add a GeometryReader
+            VStack {
+                // Header
+                ZStack {
+                    Image("login")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .edgesIgnoringSafeArea(.all)
+
+                    VStack {
+                        Text("To Do List")
+                            .font(.system(size: 50))
+                            .foregroundColor(Color.white)
                             .bold()
+
+                        Text("Get things done")
+                            .font(.system(size: 30))
+                            .foregroundColor(Color.white)
+                    }
+                    .padding(.top, -123)
+                }
+                .frame(width: geometry.size.width, height: 300) // Use geometry.size.width
+
+                // Form
+                Form {
+                    TextField("Email Address", text: $email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.top, 5)
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.top, 5)
+
+                    Button(action: {
+                        // Log in
+                    }) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(.purple)
+
+                            Text("Log in")
+                                .foregroundColor(.white)
+                                .bold()
+                        }
                     }
                 }
-            }
-            
-            // Create Account
-            VStack {
-                Text("New around here?")
-                Button("Create An Account") {
-                    // Show registration
+
+                // Create Account
+                VStack {
+                    Text("New around here?")
+                    Button("Create An Account") {
+                        // Show registration
+                    }
                 }
+                .padding(.bottom, 30)
+
+                Spacer()
             }
-            .padding(.bottom, 30)
-            
-            Spacer()
         }
     }
 }
@@ -78,4 +78,3 @@ struct Login_Previews: PreviewProvider {
         LoginView()
     }
 }
-
--- ToDoApp/Views/RegisterView.swift
+++ ToDoApp/Views/RegisterView.swift
@@ -6,3 +6,70 @@
 //
 
 import SwiftUI
+
+struct RegisterView: View {
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+
+    var body: some View {
+        GeometryReader { geometry in // Add a GeometryReader
+            VStack {
+                // Header
+                ZStack {
+                    Image("register")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .edgesIgnoringSafeArea(.all)
+
+                    VStack {
+                        Text("Register")
+                            .font(.system(size: 50))
+                            .foregroundColor(Color.white)
+                            .bold()
+
+                        Text("Begin life planning")
+                            .font(.system(size: 30))
+                            .foregroundColor(Color.white)
+                    }
+                    .padding(.top, -123)
+                }
+                .frame(width: geometry.size.width, height: 300) // Use geometry.size.width
+
+                // Form
+                Form {
+                    TextField("Full name", text: $name)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.top, 5)
+                    TextField("Email Address", text: $email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.top, 5)
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.top, 5)
+
+                    Button(action: {
+                        // Create account
+                    }) {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(.purple)
+
+                            Text("Create account")
+                                .foregroundColor(.white)
+                                .bold()
+                        }
+                    }
+                }
+
+                Spacer()
+            }
+        }
+    }
+}
+
+struct Register_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
