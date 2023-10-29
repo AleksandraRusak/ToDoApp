a709b1815aae931c38a8514a790381a1300efa23
AleksandraRusak <rusakaleksandra94@gmail.com>
2023-10-29 22:38:09 +0100

    created func fetchUser(), logOut() in ProfileView

--- ToDoApp/Views/ProfileView.swift
+++ ToDoApp/Views/ProfileView.swift
@@ -15,9 +15,50 @@ struct ProfileView: View {
         
         NavigationStack {
             VStack {
+                if let user = viewModel.user {
+                    // avatar
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(.blue)
+                        .frame(width: 125, height: 125)
+                        .padding()
+                    
+                    // user info: name, email, member since
+                    VStack(alignment: .leading) {
+                        HStack{
+                            Text("Name: ")
+                                .bold()
+                            Text(user.name)
+                        }.padding()
+                        HStack{
+                            Text("Email: ")
+                                .bold()
+                            Text(user.email)
+                        }.padding()
+                        HStack{
+                            Text("Member since: ")
+                                .bold()
+                            Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+                        }.padding()
+                    }
+                    
+                    // sign out
+                    Button("Log out") {
+                        viewModel.logOut()
+                    }
+                    .tint(.red)
+                    .padding()
+                    Spacer()
+                } else {
+                    Text("Loading Profile...")
+                }
                 
             }
             .navigationTitle("Profile")
+        }
+        .onAppear {
+            viewModel.fetchUser()
         }
     }
     
